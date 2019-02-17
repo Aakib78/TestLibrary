9a16d4b2e2926a09e12b55944b5e685280eecec6
Aakib78 <33350152+Aakib78@users.noreply.github.com>
2019-02-17 21:28:01 +0530

    Update SoobertechToast.java

--- soobertech/src/main/java/com/soober/admin/soobertech/SoobertechToast.java
+++ soobertech/src/main/java/com/soober/admin/soobertech/SoobertechToast.java
@@ -8,4 +8,12 @@ public class SoobertechToast {
     public static void toastMessage(Context c, String message){
         Toast.makeText(c, message, Toast.LENGTH_SHORT).show();
     }
+     public static void customToastMessage(Context c,String message){
+        View view =View.inflate(c,R.layout.toast_layout,null);
+        Toast toast=new Toast(c);
+        toast.setGravity(Gravity.CENTER,0,0);
+        toast.setDuration(Toast.LENGTH_LONG);
+        toast.setText(message);
+        toast.setView(view);
+        toast.show();
 }
